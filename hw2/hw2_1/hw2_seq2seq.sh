84f8d25cff2e5eab857299d25dacb296d3888d92
jasonluo <jason830201@gmail.com>
2018-05-30 14:43:35 +0800

    feature: change hw2_seq2seq.sh

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -5,7 +5,7 @@ if [ -d "./models" ]; then
 	echo "model exists"
 else
 	echo "model not exists"
-	wget --no-check-certificate "https://www.dropbox.com/s/6lziyftl2ru5tj5/models.tar.gz?dl=1" -O models.tar.gz
+	wget --no-check-certificate "https://www.dropbox.com/s/2t492mthzc9wt5x/models.tar.gz?dl=1" -O models.tar.gz
 	tar zxvf models.tar.gz
 fi
 
