6f003f8eba707f5f0b9c5bc44702b6fca3d8ab52
jasonluo <jason830201@gmail.com>
2018-06-07 12:00:59 +0800

    feature: chane testc.py

--- hw3/hw3_1/testc.py
+++ hw3/hw3_1/testc.py
@@ -2,15 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from cgan import image_gan
+from read_data_cgan import setup_dicts
+import math
 
 print("Generate images")
-batch_size = 25
+
+dicts_ = setup_dicts('./dataset/extra_data/tags.csv')
+## setup tags
+f = open('./dataset/AnimeDataset/sample_testing_text.txt', 'r')
+datas = f.readlines()
+tags0 = []
+for index, tagss in enumerate(datas):
+    tags_0 = [0] * len(dicts_)
+    tags00 = tagss.split(',')[-1].rstrip('\n')
+    tags_0[dicts_[tags00]] = 1
+    tags0.append(tags_0)
+
+batch_size = len(tags0)
 z_dimension = 100
 #z_batch0 = np.random.random([batch_size, z_dimension]) * 2 - 1 # for test images
 z_batch0 = np.random.normal(size=[batch_size, z_dimension]) # for test images
-tags00 = [0] *52 + [1] + [0] * 67
-tags0 = [tags00 for i in range(batch_size)]
-### init the model
+
+## model initiate
 gan_model = image_gan(batch_size)
 gan_model.build_model()
 saver = tf.train.Saver()
@@ -31,9 +44,10 @@ with tf.Session() as sess:
     testImage = testImage.reshape([batch_size, 64, 64, 3])
     testImage = (testImage + 1) / 2.
     for jj, im in enumerate(testImage):
-        plt.subplot(5, 5, jj+1)
+        plt.subplot(math.ceil(len(tags0)/5), 5, jj+1)
         plt.axis('off')
         plt.imshow(im)
 
-#plt.show()
-plt.savefig('../../../gan-baseline/output_cgan.png')
+plt.show()
+
+#plt.savefig('../../../gan-baseline/output_cgan.png')
